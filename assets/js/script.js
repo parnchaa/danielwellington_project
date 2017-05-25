94481c4ee1cb506383e3320afa8d5d4c7cc7a038
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2017-05-26 01:08:09 +0700

    get xml to show in webpage (htdoc >> file name) | open with xampp localhost/fileName (No spacebar)

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,7 @@ function showDetail(i) {
   "<br>Strap : " + x[i].getElementsByTagName("STRAP")[0].childNodes[0].nodeValue +
   "<br>Strap Width : " + x[i].getElementsByTagName("STRAPWIDTH")[0].childNodes[0].nodeValue +
   "<br>Strap Color : " + x[i].getElementsByTagName("STRAPCOLOR")[0].childNodes[0].nodeValue +
-  "<br>Water Resistant : " + x[i].getElementsByTagName("WATERRESISTANT")[0].childNodes[0].nodeValue +
+  "<br>Water Resistant : " + x[i].getElementsByTagName("WATERRESISTANT")[0].childNodes[0].nodeValue
   // "<br><br><img src=assets/img" +
   // x[i].getElementsByTagName("IMAGE")[0].childNodes[0].nodeValue+">";
 }
